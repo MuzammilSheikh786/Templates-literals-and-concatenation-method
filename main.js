8d18de784228ac6ca6eb0942b98580e6a1ef9825
MuzammilSheikh786 <muzammil26122004@gmail.com>
2024-09-24 15:19:35 +0500

    Template literal and concatenation method

---
+++ main.js
@@ -0,0 +1,15 @@
+"use strict";
+// Template literal Method:
+// let myAge = 19
+// let person1 = "My age is";
+// console.log(`${person1} ${myAge}`);
+// Concatenation Method:
+// let person = "zain";
+// console.log("My name is"+ ' '+person);
+// let work = "duty";
+// console.log(`I go to my ${work}`);
+// let work = "office";
+// console.log(`I go to my ${work}`);
+let x = 40;
+let b = 10;
+console.log(x > b);
---
+++ main.ts
@@ -0,0 +1,15 @@
+// Template literal Method:
+let myAge = 19
+let person1 = "My age is";
+console.log(`${person1} ${myAge}`);
+
+// Concatenation Method:
+let person = "zain";
+console.log("My name is"+ ' '+person);
+
+let work1 = "duty";
+console.log(`I go to my ${work1}`);
+
+let work2 = "office";
+console.log(`I go to my ${work2}`);
+
